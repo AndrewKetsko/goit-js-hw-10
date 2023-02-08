a3c95f8af45de7e30a7ccd3fa796c6507c3f59f4
AndrewKetsko <114005173+AndrewKetsko@users.noreply.github.com>
2023-02-08 15:46:29 +0000

    Deploying to gh-pages from @ AndrewKetsko/goit-js-hw-09-final@49f0573b64d37b1687ea028dbf5bab2c8c93a06b 🚀

---
+++ 01-color-switcher.21490517.js
@@ -0,0 +1,2 @@
+startBtn=document.querySelector(".js-start"),stopBtn=document.querySelector(".js-stop"),docBody=document.querySelector("body"),stopBtn.classList.add("disable"),startBtn.addEventListener("click",(()=>{startBtn.classList.add("disable"),stopBtn.classList.remove("disable"),timerId=setInterval((()=>{docBody.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),stopBtn.addEventListener("click",(()=>{stopBtn.classList.add("disable"),startBtn.classList.remove("disable"),clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.21490517.js.map
